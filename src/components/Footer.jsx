f67b1d27b5a1c18f994db92d9af8a490df901cae
reinaldoper <reinaldoper83@gmail.com>
2024-04-15 20:37:04 -0400

    feat: adiciona css ao footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-
+import slogan from '../assets/slogan.jpg';
 
 const Footer = () => {
   return (
-    <footer className="text-body-secondary py-5">
-      <div className="container">
+    <footer className="text-body-secondary py-5 footer">
+      <div className="container-footer">
         <p className="mb-1">All rights reserved &copy; 2024</p>
+        <img src={ slogan } alt="Your company slogan"/>
       </div>
     </footer>
   )
