20ef64a7068dfb00b340a97909a514f64fde665d
reinaldoper <reinaldoper83@gmail.com>
2024-04-20 11:06:11 -0400

    feta: adicina email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="text-body-secondary py-5 footer">
       <div className="container-footer">
-        <p className="mb-1">All rights reserved &copy; 2024</p>
+        <p className="mb-1">All rights reserved &copy; reinaldo.santos@reinaldo-dev.com</p>
         <img src={ slogan } alt="Your company slogan" title='Slogan'/>
       </div>
     </footer>
